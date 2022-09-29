780b700918c7c5bda727d2fd27a3b349785f73f0
Артем <marcus201@mail.ru>
2022-09-29 23:50:37 +0500

    ДЗ выполнено. Насчет взаимосвязи авторов и книг не совсем понятно по заданию, что имеется в виду и как это отразить в коде.

---
+++ src/Author.java
@@ -0,0 +1,15 @@
+public class Author {
+    private String authorName;
+    private String authorLastname;
+
+    public Author (String authorName,String authorLastname) {
+        this.authorName = authorName;
+        this.authorLastname = authorLastname;
+    }
+    public String getAuthorName () {
+        return this.authorName;
+    }
+    public String getAuthorLastname () {
+        return this.authorLastname;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,18 @@
+public class Book {
+   String name;
+   private int year;
+    public Book (String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
